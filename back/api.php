e7851e152ca93d670bdbffb8f24c94f23923d03f
José Ricardo Tovarg III <jose.2710.ricardo@gmail.com>
2024-12-15 19:03:33 -0400

    "Removed commented out code in api.php"

--- back/api.php
+++ back/api.php
@@ -9,12 +9,12 @@ header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 
 // Permitir encabezados específicos
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
+/*
 // Manejar solicitudes OPTIONS (preflight)
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
-}
+}*/
 
 
 header('Content-Type: application/json');
